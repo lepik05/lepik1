c54cc1640c0c1e8b193b90b02b199e1e431e1ae4
lepik05 <98818803+lepik05@users.noreply.github.com>
2022-02-01 15:13:42 +0200

    Add files via upload

---
+++ ylesanne2lepik.py
@@ -0,0 +1,96 @@
+# Harjutus 2
+# Henri Lepik
+# 01.02.22
+
+from math import *
+
+# Kolmnurga ümbermõõt
+# Henri Lepik
+# 01.02.22
+
+a,b,c = 15,16,18
+
+Kolmnurk = 15+16+18
+
+print("Kolmnurga ümbermõõt: ",Kolmnurk)
+
+# Toote hind
+# Henri Lepik
+# 01.02.22
+
+Toode = 36.75
+Soodus = 0.4
+Kogus = 3
+summa = round(Kogus*(Toode-(Toode*Soodus)), 2)
+print(Kogus, "Toote hind on",summa, "€")
+
+# Pitsa
+# Henri Lepik
+# 01.02.23
+
+soberad = 3
+Hind = 12.90
+Joot = 0.1
+summa1 = round(soberad/(Hind*Joot),2)
+print("Iga üks peab plekima ",summa1,"€")
+
+# Rulluisud
+# Henri Lepik
+# 01.02.22
+
+kiirus = 29.9 #km/h
+aeg = 24 # minut
+kuluaeg = round(((kiirus/60)*aeg),2)
+print("Rulluikude kiirus" ,str(kiirus), "km/h jõuaks kohale" ,aeg, "minutiga," ,kuluaeg, "km kaugele")
+
+# Kolmnurga hüpotenus
+# Henri Lepik
+# 01.02.22
+
+a = 16
+b = 9
+c2 = a*a+b*b
+c = round(sqrt(c2),2)
+print("Hüpetnuus on",c)
+
+# Ajateisendus
+# Henri Lepik
+# 01.02.22
+
+minutid = int(input("kirjuta minutite arv: "))
+aeg = minutid//60
+aeg2 = minutid-(aeg*60)
+print("Aeg on siis:",aeg,":",aeg2)
+
+# Arvusysteemid
+# Henri Lepik
+# 01.02.22
+
+numba = int(input("kirjuta number : "))
+numba2 = bin(numba)
+print(numba2)
+numba3 = hex(numba)
+print(numba3)
+
+# Kütus
+# Henri Lepik
+# 01.02.22
+
+kyts = int(input("kirjuta kulutatud kütus (Liitrides): "))
+maa = int(input("kirjuta selle kütusega sõidetud maa (km): "))
+kulund1 = kyts/(maa/100)
+print("kulub 100 km kohta",kulund1,"liitrit kütust")
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
