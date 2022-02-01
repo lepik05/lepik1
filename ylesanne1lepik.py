b5b9f12d015948c3b2d04063703ce5a5f203c0c0
lepik05 <98818803+lepik05@users.noreply.github.com>
2022-02-01 14:56:41 +0200

    Add files via upload

---
+++ ylesanne1lepik.py
@@ -0,0 +1,16 @@
+# Harjutus 1
+# Henri Lepik
+# 01.02.2022
+
+
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+
+
+print("  @..@")
+print(" (----)")
+print("( \__/ )")
+print("^^  \"\" ^^")
+
+a= int(input("Ruudu külje pikkus: "))
+s= a*a
+print("Vastus; ruudu pindala ", str(s)+"cm^2")
