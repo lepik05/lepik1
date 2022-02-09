5ff6e343fc4984b9968f6c3e11717030e3682ad7
lepik05 <98818803+lepik05@users.noreply.github.com>
2022-02-09 15:41:21 +0200

    Update ylesanne4lepik.py

--- ylesanne4lepik.py
+++ ylesanne4lepik.py
@@ -8,32 +8,85 @@
 
 #pisikekorrutustabel
 
+for k in range(1,11):
+    arv = 5
+    v = k * arv
+    print(f"{arv} x {k} = {v}")
 
 #viisikud
-
+    
+for i in range(1,21):
+    for j in range(5,6):
+        print('{0:>3}'.format(j*i), end=" ")
+    print()
 
 #arvamismäng
+import random
+nr = random.randint(1,50)
+loop = 1
+kord = 1
+ 
+print('Arva ära number 1-50')
+ 
+while loop == 1:
+    if kord <= 3:
+        arva = int(input('Sisesta täisarv 1-50 : '))
+    else:
+        veel=input("tahad weel w  jah/ei :")
+        if veel == "jah":        
+            kord = 0
+        else:
+            loop = 0
+        
+    if arva == nr:
+        kord += 1
+        print('Tubli, pakkumisi kokku: ',kord)
+        loop = 0
+    elif arva < nr:
+        kord += 1
+        print('Sinu pakutud arv on liiga väike')
+    else:
+        kord += 1
+        print('Sinu pakutud arv on liiga suur')
 
+#bÄnk
 
-#pank
-
-
+konto = 0
+raha = int(input("pane raha panka : "))
+intress= 0.05
+aastad= int(input("mitu aastat : "))
+konto += raha
+for i in range(aastad):
+    kasum = konto*intress
+    print(f"{i:4}{konto:10.2f} {kasum:10.2f} {konto+kasum:10.2f}")
+    konto+=kasum
+    
 #ruutude ja kuupde tabel
 
-
+for h in range(1,11):
+    print(f"{h:4}")
 
 
 #loto
-
-
+print("loto arv on:")
+import random
+arv= random.randint(10000,99999)
+print(arv)
 
 #tärnid
 
 for l in range(1,6):
-    for k in range(1,6):
-        print("*",end="")
-    print()
+    print("* "*l)
 
+for i in range(1,6):
+    for j in range(1,6):
+        print("* ",end="")
+    print()
+k=5 
+for l in range(1,6):
+    print("* "*k)
+    k=k-1
+    
 #ruut
 a = int(input("1. kylg: "))
 b = int(input("2. kylg: "))
