ac897a8449990dd9e4f3a68ed3ed28b928401520
lepik05 <98818803+lepik05@users.noreply.github.com>
2022-03-10 08:51:11 +0200

    Add files via upload

---
+++ 4ylesannet.py
@@ -0,0 +1,23 @@
+#4ylesannet
+#Henri Lepik
+#08.03.22
+
+#1.4
+
+#1.3
+input("pilvede kõrgus (km): ")
+for i in range(1):
+    if i >= 6:
+        print("need on ylemised pilved")
+    else:
+        print("need on alumised pilved")
+
+#1.2
+aasta = "2020 "
+liblikas = "teelehemosaiikliblikas"
+lausekeskosa = "Aasta liblikas on "
+lause = (f"{aasta}{lausekeskosa}{liblikas}")
+print(lause)
+
+#1.1
+print("Tere, maailm!")
