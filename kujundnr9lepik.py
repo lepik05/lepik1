30cb5248c9fbc3923272bdb64d5539e12de5f78d
lepik05 <98818803+lepik05@users.noreply.github.com>
2022-03-14 11:37:46 +0200

    Add files via upload

---
+++ kujundnr9lepik.py
@@ -0,0 +1,41 @@
+#turtelkujund
+#Henri Lepik
+#09.03.22
+
+import turtle
+
+aken = turtle.Screen()
+aken.setup(400,400)
+aken.title("Liigutame kilpkonna!")
+
+def kujund(pikkus):
+    t=turtle.Turtle()
+    for i in range (4):
+        t.forward(50)
+        t.left(90)
+        t.forward(50)
+        t.right(90)
+        t.forward(50)
+        t.left(90)
+        t.forward(50)
+        t.left(90)
+        t.forward(50)
+        t.right(90)
+        t.forward(50)
+        t.left(90)
+        t.forward(50)
+        t.left(90)
+        t.forward(50)
+        t.right(90)
+        t.forward(50)
+        t.left(90)
+        t.forward(50)
+        t.left(90)
+        t.forward(50)
+        t.right(90)
+        t.forward(50)
+        
+        
+        
+        
+kujund(100)
