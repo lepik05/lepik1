7243488f507ea3b93a0b384d4d5061674f56b5df
lepik05 <98818803+lepik05@users.noreply.github.com>
2022-03-14 10:48:36 +0200

    Add files via upload

---
+++ turtel1lepik.py
@@ -0,0 +1,69 @@
+#turtel1
+#Henri Lepik
+#09.03.22
+
+
+import turtle
+
+
+aken = turtle.Screen()
+aken.setup(300,300)
+aken.title("Liigutame kilpkonna!")
+
+k1 = turtle.Turtle()
+k1.color("green") 
+k1.penup()
+k1.left(-180)
+k1.pendown()
+k1.forward(100)
+
+k2 = turtle.Turtle()
+k2.color("red") 
+k2.penup()
+k2.left(-135)
+k2.pendown()
+k2.forward(100)
+
+k3 = turtle.Turtle()
+k3.color("purple") 
+k3.penup()
+k3.left(-90)
+k3.pendown()
+k3.forward(100)
+
+k4 = turtle.Turtle()
+k4.color("black") 
+k4.penup()
+k4.left(-225)
+k4.pendown()
+k4.forward(100)
+
+k5 = turtle.Turtle()
+k5.color("blue") 
+k5.penup()
+k5.right(-45)
+k5.pendown()
+k5.forward(100) 
+
+k6 = turtle.Turtle()
+k6.color("lime") 
+k6.penup()
+k6.right(0)
+k6.pendown()
+k6.forward(100)
+
+k7= turtle.Turtle()
+k7.color("orange") 
+k7.penup()
+k7.right(-90)
+k7.pendown()
+k7.forward(100)
+
+k8 = turtle.Turtle()
+k8.color("yellow") 
+k8.penup()
+k8.left(-45)
+k8.pendown()
+k8.forward(100)
+
+turtle.exitonclick()
